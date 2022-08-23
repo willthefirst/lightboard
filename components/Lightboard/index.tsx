d548177b79000d43cdcf4d839c566383c9849bf4
Will Moritz <willthefirst@gmail.com>
2022-08-23 10:00:55 -0700

    Initialize canvas

--- components/Lightboard/index.tsx
+++ components/Lightboard/index.tsx
@@ -1,9 +1,23 @@
+import { useEffect, useRef } from "react"
+
 type Props = {}
 
 function Lightboard({}: Props) {
+  const canvasRef = useRef<HTMLCanvasElement>(null);
+
+  useEffect(() => {
+    const ctx = canvasRef.current?.getContext('2d');
+
+    if (ctx) {
+      ctx.fillStyle = 'green';
+      ctx.fillRect(10, 10, 150, 100);
+    }
+  }, [canvasRef])
+  
+  
   return (
-    <div>I am the lightboard.</div>
+    <canvas ref={canvasRef}></canvas>
   )
 }
 
-export default Lightboard
+export default Lightboard;
